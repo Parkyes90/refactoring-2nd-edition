8e0901519d50eb05b31d6247983d6dc79d70733a
parkyes90 <parkyes90@gmail.com>
2021-12-08 00:08:42 +0900

    refactor: Remove invoice param

--- ch01/ch01_01.js
+++ ch01/ch01_01.js
@@ -1,6 +1,6 @@
-function renderPlainText(invoice, plays) {
-  let result = `청구 내역 (고객명: ${invoice.customer})\n`;
-  for (let perf of invoice.performances) {
+function renderPlainText(data, plays) {
+  let result = `청구 내역 (고객명: ${data.customer})\n`;
+  for (let perf of data.performances) {
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     }석)\n`;
@@ -51,14 +51,14 @@ function renderPlainText(invoice, plays) {
   }
   function totalVolumeCredits() {
     let result = 0;
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
       result += volumeCreditsFor(perf);
     }
     return result;
   }
   function totalAmount() {
     let result = 0;
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
       result += amountFor(perf);
     }
     return result;
@@ -66,5 +66,8 @@ function renderPlainText(invoice, plays) {
 }
 
 function statement(invoice, plays) {
-  return renderPlainText(invoice, plays);
+  const statementData = {};
+  statementData.customer = invoice.customer;
+  statementData.performances = invoice.performances;
+  return renderPlainText(statementData, plays);
 }
