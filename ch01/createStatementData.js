de357908806ce827e865788486e62436c89a6457
parkyes90 <parkyes90@gmail.com>
2021-12-11 20:20:57 +0900

    feat: Use reduce

--- ch01/createStatementData.js
+++ ch01/createStatementData.js
@@ -21,18 +21,10 @@ export function createStatementData(invoice, plays) {
     return plays[aPerformance.playID];
   }
   function totalVolumeCredits(data) {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += perf.volumeCredits;
-    }
-    return result;
+    return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
   }
   function totalAmount(data) {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += perf.amount;
-    }
-    return result;
+    return data.performances.reduce((total, p) => total + p.amount, 0);
   }
 }
 
