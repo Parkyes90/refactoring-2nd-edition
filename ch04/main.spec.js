db6194a75e23c62beeb3fdd208a5207a791c5d7d
parkyes90 <parkyes90@gmail.com>
2021-12-26 20:43:35 +0900

    chore: use common fixture

--- ch04/main.spec.js
+++ ch04/main.spec.js
@@ -1,9 +1,15 @@
 import { expect } from "chai";
 import { Province, sampleProvinceData } from "./main.js";
 describe("provide", () => {
+  let asia;
+  beforeEach(() => {
+    asia = new Province(sampleProvinceData());
+  });
   it("shortfall", () => {
-    const asia = new Province(sampleProvinceData());
-
     expect(asia.shortfall).equal(5);
+  });
+
+  it("profit", () => {
+    expect(asia.profit).equal(230);
   });
 });
