738db16129a68a8e5f4abc64daac70c1e8f2b32d
parkyes90 <parkyes90@gmail.com>
2021-12-31 18:37:24 +0900

    test: Add no producers test

--- ch04/main.spec.js
+++ ch04/main.spec.js
@@ -1,6 +1,6 @@
 import { expect } from "chai";
 import { Province, sampleProvinceData } from "./main.js";
-describe("provide", () => {
+describe("province", () => {
   let asia;
   beforeEach(() => {
     asia = new Province(sampleProvinceData());
@@ -11,5 +11,25 @@ describe("provide", () => {
 
   it("profit", () => {
     expect(asia.profit).equal(230);
+  });
+});
+
+describe("no producers", () => {
+  let noProducers;
+  beforeEach(() => {
+    const data = {
+      name: "No producers",
+      producers: [],
+      demand: 30,
+      price: 20,
+    };
+    noProducers = new Province(data);
+  });
+
+  it("shortfall", () => {
+    expect(noProducers.shortfall).equal(30);
+  });
+  it("profit", () => {
+    expect(noProducers.profit).equal(0);
   });
 });
