8b42ad41bedb968a9ea7437d99dacba00c6df52a
parkyes90 <parkyes90@gmail.com>
2022-01-03 23:22:26 +0900

    Extract print functions

--- ch06/main.js
+++ ch06/main.js
@@ -1,9 +1,7 @@
 function printOwing(invoice) {
   let outstanding = 0;
 
-  console.log("***********");
-  console.log("****고객 채무****");
-  console.log("***********");
+  printBanner();
 
   // 미해결 채무 계산
   for (const o of invoice.orders) {
@@ -18,8 +16,18 @@ function printOwing(invoice) {
     today.getDate() + 30
   );
 
-  // 세부 사항 출력
-  console.log(`고객명: ${invoice.customer}`);
-  console.log(`채무액: ${outstanding}`);
-  console.log(`마감일: ${invoice.dueDate.toLocaleDateString()}`);
+  printDetails();
+
+  function printBanner() {
+    console.log("***********");
+    console.log("****고객 채무****");
+    console.log("***********");
+  }
+
+  function printDetails() {
+    // 세부 사항 출력
+    console.log(`고객명: ${invoice.customer}`);
+    console.log(`채무액: ${outstanding}`);
+    console.log(`마감일: ${invoice.dueDate.toLocaleDateString()}`);
+  }
 }
