f744229493171070c9778015bff9303583a1ae8e
parkyes90 <parkyes90@gmail.com>
2022-01-04 22:25:18 +0900

    case 2 inline

--- ch06/inline-function.js
+++ ch06/inline-function.js
@@ -2,17 +2,9 @@ function rating(aDriver) {
   return aDriver.numberOfLateDeliveries > 5 ? 2 : 1;
 }
 
-function moreThanFiveLateDeliveries(aDriver) {
-  return aDriver.numberOfLateDeliveries > 5;
-}
-
 function reportLines(aCustomer) {
   const lines = [];
-  gatherCustomerData(lines, aCustomer);
+  lines.push(["name", aCustomer.name]);
+  lines.push(["location", aCustomer.location]);
   return lines;
-}
-
-function gatherCustomerData(out, aCustomer) {
-  out.push(["name", aCustomer.name]);
-  out.push(["location", aCustomer.location]);
 }
