3af403bb4520e2274cae3a2dee60efb62cb86b1b
parkyes90 <parkyes90@gmail.com>
2022-01-09 18:44:34 +0900

    Add base code

---
+++ ch06/combine-functions-into-transform.js
@@ -0,0 +1,12 @@
+import _ from "lodash";
+
+function base(aReading) {}
+
+function taxableCharge(aReading) {}
+
+function enrichReading(argReading) {
+  const aReading = _.cloneDeep(argReading);
+  aReading.baseCharge = base(aReading);
+  aReading.taxableCharge = taxableCharge(aReading);
+  return aReading;
+}
