321fe280a40b0748039b558c4461324f81398917
parkyes90 <parkyes90@gmail.com>
2022-01-09 19:40:34 +0900

    Add examples

--- ch06/combine-functions-into-transform.js
+++ ch06/combine-functions-into-transform.js
@@ -4,9 +4,42 @@ function base(aReading) {}
 
 function taxableCharge(aReading) {}
 
+function baseRate() {}
+function taxThreshold() {}
+
 function enrichReading(argReading) {
   const aReading = _.cloneDeep(argReading);
   aReading.baseCharge = base(aReading);
   aReading.taxableCharge = taxableCharge(aReading);
   return aReading;
 }
+
+const acquireReading = () => {
+  return {
+    customer: "ivan",
+    quantity: 10,
+    month: 5,
+    year: 2017,
+  };
+};
+
+const client1 = () => {
+  const aReading = acquireReading();
+  const baseCharge =
+    baseRate(aReading.month, aReading.year) * aReading.quantity;
+};
+
+const client2 = () => {
+  const aReading = acquireReading();
+  const baseCharge =
+    baseRate(aReading.month, aReading.year) * aReading.quantity;
+  const taxableCharge = Math.max(0, base - taxThreshold(aReading.year));
+};
+
+const client3 = () => {
+  const aReading = acquireReading();
+  const baseCharge = calculateBaseCharge(aReading);
+  function calculateBaseCharge(aReading) {
+    return baseRate(aReading.month, aReading.year) * aReading.quantity;
+  }
+};
